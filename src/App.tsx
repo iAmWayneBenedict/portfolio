1d461e78965caf7fa7bb84ffa861d40e34c62075
iAmWayne <iraolawayne@gmail.com>
2023-01-02 13:23:32 +0800

    added smooth scrolling

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import Home from "./pages/Home.page";
 import Nav from "./layouts/Home.layouts/Nav.layouts/Nav.layout";
+import gsap from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
+import LocomotiveScroll from "locomotive-scroll";
+
+// locomotive scroll css
+import "locomotive-scroll/dist/locomotive-scroll.min.css";
 
 function App() {
-	const [count, setCount] = useState(0);
-
 	const isDarkMode = () => window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)");
 
-	const icon = document.querySelector("link[rel=icon]") as HTMLLinkElement;
+	const icon: HTMLLinkElement = document.querySelector("link[rel=icon]") as HTMLLinkElement;
 
 	useEffect(() => {
 		isDarkMode().matches
@@ -15,10 +19,24 @@ function App() {
 			: icon?.setAttribute("href", "/logo-lightmode.svg");
 	}, []);
 
+	gsap.registerPlugin(ScrollTrigger);
+
+	let d = useRef<HTMLDivElement>(null);
+
+	useEffect(() => {
+		const locoScroll = new LocomotiveScroll({
+			el: d.current as HTMLDivElement,
+			smooth: true,
+			multiplier: 0.75,
+		});
+	}, []);
+
 	return (
-		<div className="App">
-			<Nav />
-			<Home />
+		<div className="App" ref={d}>
+			<div id="smooth-wrapper">
+				<Nav />
+				<Home />
+			</div>
 		</div>
 	);
 }
--- src/pages/Home.page.tsx
+++ src/pages/Home.page.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import AboutSection from "../layouts/Home.layouts/About.layouts/AboutSection.layout";
+import Certificates from "../layouts/Home.layouts/Certificates.layouts/Certificates.layout";
+import Designs from "../layouts/Home.layouts/Designs.layouts/Designs.layout";
 import Hero from "../layouts/Home.layouts/Hero.layouts/Hero.layout";
 import Journey from "../layouts/Home.layouts/Journey.layouts/Journey.layout";
 import Projects from "../layouts/Home.layouts/Projects.layouts/Projects.layouts";
@@ -13,6 +15,8 @@ const Home = () => {
 			<Skills />
 			<Projects />
 			<Journey />
+			<Designs />
+			<Certificates />
 		</div>
 	);
 };
