5c0975939acd8e3a541a086ea3cf32c9f19cba5b
iAmWayneBenedict <iraolawayne@gmail.com>
2023-06-16 23:14:42 +0800

    fixed cursor smooth animation

--- src/components/ui/Cursor.component.tsx
+++ src/components/ui/Cursor.component.tsx
@@ -61,9 +61,16 @@ const Cursor = () => {
 		};
 		followMouse();
 
-		let links = document.querySelectorAll("a, button");
+		let links = document.querySelectorAll("a, button, .swiper");
+		// const transitionEndHandler = (el:HTMLDivElement) => {
+		// 	el.addEventListener('transitionend', transitionHandler)
+		// 	function transitionHandler() {
+
+		// 	}
+		// }
 		links.forEach((el) => {
-			el.addEventListener("mouseover", () => {
+			el.addEventListener("mouseover", (event) => {
+				let currentEl = event.currentTarget as Element;
 				if (
 					!secondaryCursor.current ||
 					!mainCursor.current ||
@@ -71,8 +78,16 @@ const Cursor = () => {
 				)
 					return;
 				let child = mainCursor.current.firstElementChild as HTMLDivElement;
-				child.style.width = "10px";
-				child.style.height = "10px";
+				if (currentEl.tagName === "DIV") {
+					child.style.transform = "scale(5)";
+
+					(child.nextElementSibling as HTMLSpanElement)!.style.transition =
+						"all 1s ease 500ms";
+					(child.nextElementSibling as HTMLSpanElement)!.style.opacity = "1";
+				} else {
+					child.style.transform = "scale(.45)";
+				}
+
 				secondaryCursor.current.style.opacity = "0";
 			});
 			el.addEventListener("mouseout", () => {
@@ -83,8 +98,9 @@ const Cursor = () => {
 				)
 					return;
 				let child = mainCursor.current.firstElementChild as HTMLDivElement;
-				child.style.width = "30px";
-				child.style.height = "30px";
+				child.style.transform = "scale(1)";
+				(child.nextElementSibling as HTMLSpanElement)!.style.opacity = "0";
+				child.nextElementSibling!.textContent = "";
 				secondaryCursor.current.style.opacity = "1";
 			});
 		});
@@ -101,42 +117,11 @@ const Cursor = () => {
 		if (!mainCursor.current || !secondaryCursor.current) return;
 		positionRef.current.mouseX = mouseX - secondaryCursor.current.clientWidth / 2;
 		positionRef.current.mouseY = mouseY - secondaryCursor.current.clientHeight / 2;
-		mainCursor.current.style.transform = `translate3d(${
-			mouseX - mainCursor.current.clientWidth / 2
-		}px, ${mouseY - mainCursor.current.clientHeight / 2}px, 0)`;
-	};
-
-	const mouseTwo = (event: MouseEvent) => {
-		const { clientX, clientY } = event;
-
-		setIsMoving(true);
-
-		const mouseX = clientX;
-		const mouseY = clientY;
-
-		if (!mainCursor2.current) return;
-		mainCursor2.current.style.transform = `translate3d(${
-			mouseX - mainCursor2.current.clientWidth / 2
-		}px, ${mouseY - mainCursor2.current.clientHeight / 2}px, 0)`;
-	};
-
-	const mouseThree = (event: MouseEvent) => {
-		const { clientX, clientY } = event;
-
-		setIsMoving(true);
-
-		const mouseX = clientX;
-		const mouseY = clientY;
-
-		if (!mainCursor3.current) return;
-		mainCursor3.current.style.transform = `translate3d(${
-			mouseX - mainCursor3.current.clientWidth / 2
-		}px, ${mouseY - mainCursor3.current.clientHeight / 2}px, 0)`;
+		mainCursor.current.style.top = `${mouseY - mainCursor.current.clientHeight / 2}px`;
+		mainCursor.current.style.left = `${mouseX - mainCursor.current.clientWidth / 2}px`;
 	};
 
 	useEffect(() => {
-		let statusCursor = "default";
-
 		const handleMouseMove = (event: MouseEvent) => {
 			let targetEl = event.target as HTMLElement;
 			let firstParent = targetEl.parentElement;
@@ -151,21 +136,24 @@ const Cursor = () => {
 
 			let isDesign = targetEl.tagName === "IMG" && targetEl.classList.contains("view-design");
 
-			// if (!mainCursor.current || !mainCursor2.current || !secondaryCursor.current) return;
+			if (!mainCursor.current || !secondaryCursor.current) return;
 
+			let mainCursorFChild = mainCursor.current!.firstElementChild as HTMLDivElement;
+			let mainCursorLChild = mainCursor.current!.lastElementChild as HTMLDivElement;
+
+			mainCursor.current!.style.mixBlendMode = "unset";
+			mainCursorFChild.style.background = "black";
+			mainCursorLChild.style.color = "white";
 			if (isJourney) {
-				statusCursor = "img";
-				setCursorStatus("img");
-				mouseTwo(event);
+				mainCursorLChild.textContent = "DRAG";
 			} else if (isDesign) {
-				statusCursor = "view";
-				setCursorStatus("view");
-				mouseThree(event);
+				mainCursorLChild.textContent = "VIEW";
 			} else {
-				statusCursor = "default";
-				setCursorStatus("default");
-				mouseOne(event);
+				mainCursorFChild.style.background = "white";
+				mainCursorLChild.style.color = "black";
+				mainCursor.current!.style.mixBlendMode = "difference";
 			}
+			mouseOne(event);
 		};
 
 		document.addEventListener("mousemove", handleMouseMove);
@@ -175,40 +163,40 @@ const Cursor = () => {
 		};
 	}, []);
 
-	useEffect(() => {
-		if (cursorStatus === "img") {
-			gsap.to(mainCursor2.current?.firstElementChild!, {
-				opacity: 1,
-				width: "150px",
-				height: "150px",
-				duration: 0.15,
-			});
-		} else if (cursorStatus === "view") {
-			gsap.to(mainCursor3.current?.firstElementChild!, {
-				opacity: 1,
-				width: "150px",
-				height: "150px",
-				duration: 0.15,
-			});
-		}
-	}, [cursorStatus]);
+	// useEffect(() => {
+	// 	if (cursorStatus === "img") {
+	// 		gsap.to(mainCursor2.current?.firstElementChild!, {
+	// 			opacity: 1,
+	// 			width: "150px",
+	// 			height: "150px",
+	// 			duration: 0.15,
+	// 		});
+	// 	} else if (cursorStatus === "view") {
+	// 		gsap.to(mainCursor3.current?.firstElementChild!, {
+	// 			opacity: 1,
+	// 			width: "150px",
+	// 			height: "150px",
+	// 			duration: 0.15,
+	// 		});
+	// 	}
+	// }, [cursorStatus]);
+
+	// useEffect(() => {
+	// 	// if (cursorStatus !== "default") return;
+	// 	if (!isMoving) return;
+
+	// 	gsap.to(mainCursor.current?.firstElementChild!, {
+	// 		opacity: 1,
+	// 		duration: 0,
+	// 	});
+	// 	gsap.to(secondaryCursor.current!, {
+	// 		opacity: 1,
+	// 		duration: 0,
+	// 	});
+	// }, [cursorStatus]);
 
 	useEffect(() => {
-		// if (cursorStatus !== "default") return;
 		handleMouseCursor();
-		if (!isMoving) return;
-
-		gsap.to(mainCursor.current?.firstElementChild!, {
-			opacity: 1,
-			duration: 0,
-		});
-		gsap.to(secondaryCursor.current!, {
-			opacity: 1,
-			duration: 0,
-		});
-	}, [cursorStatus]);
-
-	useEffect(() => {
 		if (!isMoving) return;
 
 		mainCursor.current?.firstElementChild?.classList.remove("opacity-0");
@@ -216,45 +204,57 @@ const Cursor = () => {
 	}, [isMoving]);
 	return (
 		<>
-			{cursorStatus === "default" && (
-				<>
-					<div
-						className="fixed mix-blend-difference z-[1000] pointer-events-none "
-						ref={mainCursor}
+			{/* {cursorStatus === "default" && ( */}
+			<>
+				<div
+					className="fixed mix-blend-difference z-[1000] pointer-events-none w-fit main-cursor"
+					ref={mainCursor}
+				>
+					<div className="bg-white w-[30px] h-[30px] rounded-full transition-transform duration-500 opacity-0 relative"></div>
+					<span
+						style={{
+							transform: "translate(-50%, -50%)",
+							transition: "none",
+							position: "absolute",
+							top: "50%",
+							left: "50%",
+							opacity: 0,
+						}}
 					>
-						<div className="bg-white w-[30px] h-[30px] rounded-full transition-all duration-500  opacity-0"></div>
-					</div>
-					<div
-						className="fixed mix-blend-difference pointer-events-none z-[1000] transition-[opacity] duration-500 opacity-0"
-						ref={secondaryCursor}
-					>
-						<div className="w-[100px] animate-[spin_10s_linear_infinite]">
-							<svg xmlns="http://www.w3.org/2000/svg" lang="en" viewBox="0 0 500 500">
-								<defs>
-									<path
-										id="textcircle"
-										d="M250,400
+						asdasd
+					</span>
+				</div>
+				<div
+					className="fixed mix-blend-difference pointer-events-none z-[1000] transition-[opacity] duration-500 opacity-0"
+					ref={secondaryCursor}
+				>
+					<div className="w-[100px] animate-[spin_10s_linear_infinite]">
+						<svg xmlns="http://www.w3.org/2000/svg" lang="en" viewBox="0 0 500 500">
+							<defs>
+								<path
+									id="textcircle"
+									d="M250,400
         a150,150 0 0,1 0,-300a150,150 0 0,1 0,300Z"
-										transform="rotate(12,250,250)"
-									/>
-								</defs>
-								<rect width="100%" height="100%" fill="none" />
-								<text className="text-[4.6rem] font-bold" style={{ fill: "white" }}>
-									<textPath
-										href="#textcircle"
-										aria-label="All for One &amp; One for All"
-										textLength="942"
-									>
-										.WAYNE. .WAYNE. .WAYNE.
-									</textPath>
-								</text>
-							</svg>
-						</div>
+									transform="rotate(12,250,250)"
+								/>
+							</defs>
+							<rect width="100%" height="100%" fill="none" />
+							<text className="text-[4.6rem] font-bold" style={{ fill: "white" }}>
+								<textPath
+									href="#textcircle"
+									aria-label="All for One &amp; One for All"
+									textLength="942"
+								>
+									.WAYNE. .WAYNE. .WAYNE.
+								</textPath>
+							</text>
+						</svg>
 					</div>
-				</>
-			)}
+				</div>
+			</>
+			{/* // )} */}
 
-			{cursorStatus === "img" && (
+			{/* {cursorStatus === "img" && (
 				<>
 					<div className="fixed z-[1000] pointer-events-none cursor-2" ref={mainCursor2}>
 						<div className="bg-black text-white w-[100px] h-[100px] rounded-full opacity-0 flex justify-center items-center">
@@ -271,7 +271,7 @@ const Cursor = () => {
 						</div>
 					</div>
 				</>
-			)}
+			)} */}
 		</>
 	);
 };
--- src/utils/handleTouchDragEvent.ts
+++ src/utils/handleTouchDragEvent.ts
@@ -2,51 +2,39 @@ let isSwiping = false;
 let startX = 0;
 let startY = 0;
 
-const handleTouchStart = ({
-                              currentX,
-                              currentY,
-                          }:{
-    currentX: number;
-    currentY: number;
-}) => {
-    isSwiping = true;
-    startX = currentX;
-    startY = currentY;
+const handleTouchStart = ({ currentX, currentY }: { currentX: number; currentY: number }) => {
+	isSwiping = true;
+	startX = currentX;
+	startY = currentY;
 };
 
-const handleTouchMove = ({
-                             currentX,
-                             currentY,
-                         }:{
-    currentX: number;
-    currentY: number;
-}, element: string) => {
-    if (isSwiping) {
-        const mainCursor = document.querySelector(element) as HTMLDivElement
-        const mouseThree = () => {
+const handleTouchMove = (
+	{
+		currentX,
+		currentY,
+	}: {
+		currentX: number;
+		currentY: number;
+	},
+	element: string
+) => {
+	if (isSwiping) {
+		const mainCursor = document.querySelector(element) as HTMLDivElement;
+		const mouseThree = () => {
+			const mouseX = currentX;
+			const mouseY = currentY;
 
+			if (!mainCursor) return;
 
-            const mouseX = currentX;
-            const mouseY = currentY;
-
-            if (!mainCursor) return;
-            mainCursor.style.transform = `translate3d(${
-                mouseX - mainCursor.clientWidth / 2
-            }px, ${mouseY - mainCursor.clientHeight / 2}px, 0)`;
-        };
-        mouseThree();
-    }
+			mainCursor.style.left = `${mouseX - mainCursor.clientWidth / 2}px`;
+			mainCursor.style.top = `${mouseY - mainCursor.clientHeight / 2}px`;
+		};
+		mouseThree();
+	}
 };
 
 const handleTouchEnd = () => {
-    isSwiping = false;
+	isSwiping = false;
 };
 
-export {
-    isSwiping,
-    startX,
-    startY,
-    handleTouchStart,
-    handleTouchMove,
-    handleTouchEnd
-}
+export { isSwiping, startX, startY, handleTouchStart, handleTouchMove, handleTouchEnd };
