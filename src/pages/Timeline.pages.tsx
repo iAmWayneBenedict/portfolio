4ad32ff3ac5e273a3486149b9d7679fa7264e719
iAmWayneBenedict <iraolawayne@gmail.com>
2023-07-24 13:57:29 +0800

    added helmet forSEO

--- src/pages/Timeline.pages.tsx
+++ src/pages/Timeline.pages.tsx
@@ -3,6 +3,7 @@ import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import SplitType from "split-type";
 import { ContactLayouts } from "../layouts/Home.layouts/Contact/Contact.layouts";
+import { Helmet } from "react-helmet-async";
 
 const Timeline = () => {
 	gsap.registerPlugin(ScrollTrigger);
@@ -99,108 +100,119 @@ const Timeline = () => {
 		});
 	}, []);
 	return (
-		<div className="pt-48 sm:pt-56 2xl:pt-96">
-			<div className="px-5 md:px-24 text-white">
-				<h1
-					ref={title}
-					className="overflow-hidden block relative max-w-7xl font-light"
-					style={{ fontSize: `clamp(2.5rem, 8vw, 5rem)` }}
-				>
-					<p className="title-text leading-1 uppercase">
-						Nurturing breakthroughs into a foundation for growth.
+		<>
+			<Helmet>
+				<title>Timeline</title>
+				<meta name="description" content="Discover the experiences I've gained." />
+				<link rel="canonical" href="/timeline" />
+			</Helmet>
+			<div className="pt-48 sm:pt-56 2xl:pt-96">
+				<div className="px-5 md:px-24 text-white">
+					<h1
+						ref={title}
+						className="overflow-hidden block relative max-w-7xl font-light"
+						style={{ fontSize: `clamp(2.5rem, 8vw, 5rem)` }}
+					>
+						<p className="title-text leading-1 uppercase">
+							Nurturing breakthroughs into a foundation for growth.
+						</p>
+					</h1>
+					<p className="mt-16 text-lg lg:text-xl">
+						Transforms experiences into knowledge.
 					</p>
-				</h1>
-				<p className="mt-16 text-lg lg:text-xl">Transforms experiences into knowledge.</p>
+				</div>
+				<table
+					ref={body}
+					className="border-collapse border-0 md:border-y-[1px] border-[#4F4F4F] w-full mt-48"
+				>
+					<tbody className="text-white">
+						<tr className="border-0 md:border-y-[1px] border-[#4F4F4F]">
+							<td className="py-10 px-5 md:px-20 hidden lg:flex flex-col w-[25rem]">
+								<h1 className="text-6xl font-bold h-full">2023</h1>
+								<h1 className="text-xl h-full mt-5">March - June</h1>
+							</td>
+							<td className="py-10 px-5 md:px-20 border-0 md:border-y-[1px] border-l-[1px] border-[#4F4F4F]">
+								<div className="w-full flex justify-end">
+									<h6 className="text-xl md:text-2xl italic text-[#858585] hidden md:block">
+										Intern
+									</h6>
+								</div>
+								<h1 className="mb-3 text-3xl md:text-6xl font-bold h-full block md:hidden">
+									2023
+								</h1>
+								<h3 className="text-3xl md:text-5xl">Web Developer</h3>
+								<h5 className="text-sm xl:text-lg text-[#858585] max-w-5xl">
+									Department of Information and Communication Technology Region V
+									Camarines Sur Provincial Office
+								</h5>
+								<p className="mt-5 max-w-5xl text-base xl:text-xl text-[#d8d8d8]">
+									Lorem ipsum dolor sit amet consectetur adipisicing elit.
+									Dignissimos velit fugit alias, ab nisi ducimus, minus facilis
+									exercitationem nam deserunt recusandae adipisci nesciunt,
+									voluptates non. Ea aliquid corrupti numquam culpa.
+								</p>
+							</td>
+						</tr>
+						<tr className="border-0 md:border-y-[1px] border-[#4F4F4F]">
+							<td className="py-10 px-5 md:px-20 hidden lg:flex flex-col w-[25rem]">
+								<h1 className="text-6xl font-bold h-full">2023</h1>
+								<h1 className="text-xl h-full mt-5">August 2019 - July 2023</h1>
+							</td>
+							<td className="py-10 px-5 md:px-20 border-0 md:border-y-[1px] border-l-[1px] border-[#4F4F4F]">
+								<div className="w-full flex justify-end">
+									<h6 className="text-xl md:text-2xl italic text-[#858585] hidden md:block">
+										College
+									</h6>
+								</div>
+								<h1 className="mb-3 text-3xl md:text-6xl font-bold h-full block md:hidden">
+									2023
+								</h1>
+								<h3 className="text-3xl md:text-5xl">
+									BS in Information Technology
+								</h3>
+								<h5 className="text-sm xl:text-lg text-[#858585] max-w-5xl">
+									Camarines Sur Polytechnic Colleges
+								</h5>
+								<p className="mt-5 max-w-5xl text-base xl:text-xl text-[#d8d8d8]">
+									Lorem ipsum dolor sit amet consectetur adipisicing elit.
+									Dignissimos velit fugit alias, ab nisi ducimus, minus facilis
+									exercitationem nam deserunt recusandae adipisci nesciunt,
+									voluptates non. Ea aliquid corrupti numquam culpa.
+								</p>
+							</td>
+						</tr>
+						<tr className="border-0 md:border-y-[1px] border-[#4F4F4F]">
+							<td className="py-10 px-5 md:px-20 hidden lg:flex flex-col w-[25rem]">
+								<h1 className="text-6xl font-bold h-full">2019</h1>
+								<h1 className="text-xl h-full mt-5">July 2017 - May 2019</h1>
+							</td>
+							<td className="py-10 px-5 md:px-20 border-0 md:border-y-[1px] border-l-[1px] border-[#4F4F4F]">
+								<div className="w-full flex justify-end">
+									<h6 className="text-xl md:text-2xl italic text-[#858585] hidden md:block">
+										Senior High
+									</h6>
+								</div>
+								<h1 className="mb-3 text-3xl md:text-6xl font-bold h-full block md:hidden">
+									2019
+								</h1>
+								<h3 className="text-3xl md:text-5xl">General Academic Strand</h3>
+								<h5 className="text-sm xl:text-lg text-[#858585] max-w-5xl">
+									ACLC College of Iriga, Inc.
+								</h5>
+								<p className="mt-5 max-w-5xl text-base xl:text-xl text-[#d8d8d8]">
+									Lorem ipsum dolor sit amet consectetur adipisicing elit.
+									Dignissimos velit fugit alias, ab nisi ducimus, minus facilis
+									exercitationem nam deserunt recusandae adipisci nesciunt,
+									voluptates non. Ea aliquid corrupti numquam culpa.
+								</p>
+							</td>
+						</tr>
+					</tbody>
+				</table>
+				<br />
+				<ContactLayouts />
 			</div>
-			<table
-				ref={body}
-				className="border-collapse border-0 md:border-y-[1px] border-[#4F4F4F] w-full mt-48"
-			>
-				<tbody className="text-white">
-					<tr className="border-0 md:border-y-[1px] border-[#4F4F4F]">
-						<td className="py-10 px-5 md:px-20 hidden lg:flex flex-col w-[25rem]">
-							<h1 className="text-6xl font-bold h-full">2023</h1>
-							<h1 className="text-xl h-full mt-5">March - June</h1>
-						</td>
-						<td className="py-10 px-5 md:px-20 border-0 md:border-y-[1px] border-l-[1px] border-[#4F4F4F]">
-							<div className="w-full flex justify-end">
-								<h6 className="text-xl md:text-2xl italic text-[#858585] hidden md:block">
-									Intern
-								</h6>
-							</div>
-							<h1 className="mb-3 text-3xl md:text-6xl font-bold h-full block md:hidden">
-								2023
-							</h1>
-							<h3 className="text-3xl md:text-5xl">Web Developer</h3>
-							<h5 className="text-sm xl:text-lg text-[#858585] max-w-5xl">
-								Department of Information and Communication Technology Region V
-								Camarines Sur Provincial Office
-							</h5>
-							<p className="mt-5 max-w-5xl text-base xl:text-xl text-[#d8d8d8]">
-								Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos
-								velit fugit alias, ab nisi ducimus, minus facilis exercitationem nam
-								deserunt recusandae adipisci nesciunt, voluptates non. Ea aliquid
-								corrupti numquam culpa.
-							</p>
-						</td>
-					</tr>
-					<tr className="border-0 md:border-y-[1px] border-[#4F4F4F]">
-						<td className="py-10 px-5 md:px-20 hidden lg:flex flex-col w-[25rem]">
-							<h1 className="text-6xl font-bold h-full">2023</h1>
-							<h1 className="text-xl h-full mt-5">August 2019 - July 2023</h1>
-						</td>
-						<td className="py-10 px-5 md:px-20 border-0 md:border-y-[1px] border-l-[1px] border-[#4F4F4F]">
-							<div className="w-full flex justify-end">
-								<h6 className="text-xl md:text-2xl italic text-[#858585] hidden md:block">
-									College
-								</h6>
-							</div>
-							<h1 className="mb-3 text-3xl md:text-6xl font-bold h-full block md:hidden">
-								2023
-							</h1>
-							<h3 className="text-3xl md:text-5xl">BS in Information Technology</h3>
-							<h5 className="text-sm xl:text-lg text-[#858585] max-w-5xl">
-								Camarines Sur Polytechnic Colleges
-							</h5>
-							<p className="mt-5 max-w-5xl text-base xl:text-xl text-[#d8d8d8]">
-								Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos
-								velit fugit alias, ab nisi ducimus, minus facilis exercitationem nam
-								deserunt recusandae adipisci nesciunt, voluptates non. Ea aliquid
-								corrupti numquam culpa.
-							</p>
-						</td>
-					</tr>
-					<tr className="border-0 md:border-y-[1px] border-[#4F4F4F]">
-						<td className="py-10 px-5 md:px-20 hidden lg:flex flex-col w-[25rem]">
-							<h1 className="text-6xl font-bold h-full">2019</h1>
-							<h1 className="text-xl h-full mt-5">July 2017 - May 2019</h1>
-						</td>
-						<td className="py-10 px-5 md:px-20 border-0 md:border-y-[1px] border-l-[1px] border-[#4F4F4F]">
-							<div className="w-full flex justify-end">
-								<h6 className="text-xl md:text-2xl italic text-[#858585] hidden md:block">
-									Senior High
-								</h6>
-							</div>
-							<h1 className="mb-3 text-3xl md:text-6xl font-bold h-full block md:hidden">
-								2019
-							</h1>
-							<h3 className="text-3xl md:text-5xl">General Academic Strand</h3>
-							<h5 className="text-sm xl:text-lg text-[#858585] max-w-5xl">
-								ACLC College of Iriga, Inc.
-							</h5>
-							<p className="mt-5 max-w-5xl text-base xl:text-xl text-[#d8d8d8]">
-								Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos
-								velit fugit alias, ab nisi ducimus, minus facilis exercitationem nam
-								deserunt recusandae adipisci nesciunt, voluptates non. Ea aliquid
-								corrupti numquam culpa.
-							</p>
-						</td>
-					</tr>
-				</tbody>
-			</table>
-			<br />
-			<ContactLayouts />
-		</div>
+		</>
 	);
 };
 
